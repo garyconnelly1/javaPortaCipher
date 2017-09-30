18e4ff10e4c085e4ed302260aecae8395587b116
ZqhKHP <G00336837@gmit.ie>
2017-09-30 21:12:45 +0100

    starting parser class

--- Parser.java
+++ Parser.java
@@ -6,3 +6,13 @@ import java.io.*;
 //import java.net.MalformedURLException;
 //import java.net.URL;
 import java.net.*;
+
+public class Parser {
+	
+	private static long benchmarkTime = 0;
+	
+	//method to create the buffer reader
+	public static BufferedReader myParser(String location)
+	{
+		//initialise buffered reader
+		BufferedReader br = null;
