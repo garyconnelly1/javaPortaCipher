f40f43bd0607cfeeaf841d04e0224f84a3881769
ZqhKHP <G00336837@gmit.ie>
2017-09-30 21:18:36 +0100

    try statement

--- Parser.java
+++ Parser.java
@@ -46,3 +46,23 @@ public class Parser {
 		BufferedReader br = myParser(location);
 		
 		int x;
+		
+		try
+		{
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));//To write the contents of the file to a file "output.txt" 
+			
+			//while loop
+			//-1 means the we are at the end of the file
+			benchmarkTime = System.currentTimeMillis();
+			while((x = br.read()) != -1)
+			{
+				//System.out.println("This is working:");
+				char c = Decryption.myDecrypter(Character.toUpperCase((char)x), password);
+				//System.out.println(c);
+				bw.write(c);//write each character as upper case to the output file
+			}//end while. writes to file in linear time.O(n).
+			System.out.println("Encryption time (in milliseconds): "+(System.currentTimeMillis() - benchmarkTime));
+			
+			br.close();//close bufferReader
+			bw.close();//close bufferWriter
+		}
