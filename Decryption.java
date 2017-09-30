211c1e01fcddeb856c228faa9b0d457f64ba92f2
ZqhKHP <G00336837@gmit.ie>
2017-09-30 21:26:09 +0100

    adding Decryption class

--- Decryption.java
+++ Decryption.java
@@ -4,3 +4,12 @@
 package ie.gmit.sw;
 
 import java.util.List;
+
+public class Decryption {
+	
+	//private static int q = 0;
+	//private static int col = 0;
+	//private static int row = 0;
+	
+	//initialise pass position
+	private static int passPosition = 0;
